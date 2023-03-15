fb5f79cb7f4561271cc2508342baa176ec1d1222
taskie <t@skie.jp>
2023-03-16 08:40:26 +0900

    feat!: add `run-console` subcommand

---
+++ src/console.rs
@@ -0,0 +1,93 @@
+use std::io::{BufRead, Write};
+
+use anyhow::{Context as _, Result};
+use log::{debug, warn};
+use rexpect::{reader::Regex, spawn_bash};
+
+#[derive(Clone, Debug)]
+struct Document {
+    entries: Vec<Entry>,
+}
+
+#[derive(Clone, Debug)]
+struct Entry {
+    command: String,
+    expected: Option<String>,
+}
+
+fn parse_lines_as_document<I: Iterator<Item = String>>(iter: I) -> Document {
+    let mut entries = vec![];
+    let mut command_lines = vec![];
+    let mut output_lines = vec![];
+    let mut push_entry = |command_lines: &mut Vec<String>, output_lines: &mut Vec<String>| {
+        if !command_lines.is_empty() {
+            let expected = if output_lines.is_empty() {
+                None
+            } else {
+                Some(output_lines.join("\n") + "\n")
+            };
+            let entry = Entry {
+                command: command_lines.join("\n"),
+                expected,
+            };
+            entries.push(entry);
+        }
+        command_lines.clear();
+        output_lines.clear();
+    };
+    let mut cmd_continues = false;
+    for l in iter {
+        let cmd_starts = !cmd_continues && l.starts_with('$');
+        let is_cmd = cmd_starts || cmd_continues;
+        if is_cmd {
+            if !cmd_continues {
+                push_entry(&mut command_lines, &mut output_lines);
+            }
+            let start: usize = usize::from(cmd_starts);
+            cmd_continues = is_cmd && l.ends_with('\\');
+            let end: usize = if cmd_continues { l.len() - 1 } else { l.len() };
+            let command_line = l[start..end].to_owned();
+            command_lines.push(command_line);
+        } else {
+            output_lines.push(l);
+        }
+    }
+    push_entry(&mut command_lines, &mut output_lines);
+    Document { entries }
+}
+
+fn parse_document<R: BufRead>(r: R) -> Result<Document> {
+    itertools::process_results(r.lines(), |iter| parse_lines_as_document(iter))
+        .context("can't read input")
+}
+
+pub(crate) fn run_console<R: BufRead, W: Write>(
+    r: R,
+    mut w: W,
+    timeout: Option<u64>,
+) -> Result<()> {
+    let ansi_color_regex = Regex::new("\u{1b}\\[[0-9;]*m").unwrap();
+    let document = parse_document(r)?;
+    debug!("{:?}", document);
+    let mut p = spawn_bash(timeout)?;
+    // workaround
+    p.execute("bind 'set enable-bracketed-paste off'", ".*?")?;
+    p.wait_for_prompt()?;
+    for entry in document.entries.iter() {
+        w.write_all(b"$")?;
+        w.write_all(entry.command.as_bytes())?;
+        w.write_all(b"\n")?;
+        p.execute(&entry.command, ".*?")?;
+        let actual = p.wait_for_prompt()?;
+        let actual = ansi_color_regex.replace_all(&actual, "");
+        let actual = actual.replace('\r', "");
+        if let Some(expected) = entry.expected.as_ref() {
+            if expected != &actual {
+                warn!("expected: \n{}", expected.trim_end());
+            }
+        }
+        w.write_all(actual.as_bytes())?;
+    }
+    p.execute("exit", ".*?")?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::fs::File;
-use std::io::BufReader;
-use std::path::PathBuf;
+use std::io::{stdin, stdout, BufReader};
+use std::path::{Path, PathBuf};
 
 use anyhow::Result;
 use clap::Parser;
@@ -10,11 +10,24 @@ use crate::config::Config;
 
 mod cmark;
 mod config;
+mod console;
 mod executor;
 
 #[derive(Debug, Clone, Parser)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
+    #[clap(subcommand)]
+    subcommand: Subcommand,
+}
+
+#[derive(Debug, Clone, clap::Subcommand)]
+enum Subcommand {
+    Run(RunArgs),
+    RunConsole(RunConsoleArgs),
+}
+
+#[derive(Debug, Clone, clap::Args)]
+struct RunArgs {
     #[clap(long = "no-caption", action = clap::ArgAction::SetFalse)]
     caption: bool,
     #[clap(long = "caption", overrides_with = "caption")]
@@ -23,18 +36,50 @@ struct Args {
     files: Vec<PathBuf>,
 }
 
-fn main() -> Result<()> {
-    env_logger::init();
-    let args = Args::parse();
-    debug!("{:?}", args);
-    for file in args.files.iter() {
+#[derive(Debug, Clone, clap::Args)]
+#[clap(author, version, about, long_about = None)]
+struct RunConsoleArgs {
+    #[clap(short, long)]
+    timeout: Option<u64>,
+    #[clap(name = "FILE")]
+    file: Option<PathBuf>,
+}
+
+fn run(_args: &Args, subargs: &RunArgs) -> Result<()> {
+    for file in subargs.files.iter() {
         let f = File::open(file)?;
         let br = BufReader::new(f);
         let conf = Config {
-            caption: Some(args.caption),
+            caption: Some(subargs.caption),
             ..Default::default()
         };
         cmark::process(br, &conf)?;
     }
     Ok(())
+}
+
+fn console(_args: &Args, subargs: &RunConsoleArgs) -> Result<()> {
+    let stdout = stdout().lock();
+    let file = subargs.file.clone().unwrap_or_else(|| "-".into());
+    let timeout_ms = subargs.timeout.map(|v| v * 1000);
+    if file.as_path() == Path::new("-") {
+        let stdin = stdin().lock();
+        let br = BufReader::new(stdin);
+        console::run_console(br, stdout, timeout_ms)?;
+    } else {
+        let f = File::open(file)?;
+        let br = BufReader::new(f);
+        console::run_console(br, stdout, timeout_ms)?;
+    }
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    env_logger::init();
+    let args = Args::parse();
+    debug!("{:?}", args);
+    match &args.subcommand {
+        Subcommand::Run(subargs) => run(&args, subargs),
+        Subcommand::RunConsole(subargs) => console(&args, subargs),
+    }
 }
